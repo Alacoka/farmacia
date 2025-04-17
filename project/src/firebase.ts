f38cb69f1f1121fa040422bb7df0d1e4d0ab3cd3
Alacoque <kawaalacoque@gmail.com>
2025-04-17 10:36:08 -0300

    updates pt2

--- project/src/firebase.ts
+++ project/src/firebase.ts
@@ -2,15 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD1BJeehCrDqeyRTN4NCsx5sSgrH6-dcW4",
-    authDomain: "farmacia-ad684.firebaseapp.com",
-    projectId: "farmacia-ad684",
-    storageBucket: "farmacia-ad684.firebasestorage.app",
-    messagingSenderId: "1024282036794",
-    appId: "1:1024282036794:web:cb33d89928b43e8d42c303",
-    measurementId: "G-8J9ED4DHYY"
-  };
+export const firebaseConfig = {
+  apiKey: "AIzaSyD1BJeehCrDqeyRTN4NCsx5sSgrH6-dcW4",
+  authDomain: "farmacia-ad684.firebaseapp.com",
+  projectId: "farmacia-ad684",
+  storageBucket: "farmacia-ad684.firebasestorage.app",
+  messagingSenderId: "1024282036794",
+  appId: "1:1024282036794:web:cb33d89928b43e8d42c303",
+  measurementId: "G-8J9ED4DHYY"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
